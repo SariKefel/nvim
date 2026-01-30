6e6ac4ad5cde80984642e9f2b613fed3bdac07b8
Sari Kefel <sarikefel@gmail.com>
2026-01-30 11:07:12 +0800

    Add modern LSP and formatters for React/Next.js development
    - Add ts_ls, tailwindcss, eslint language servers
    - Configure prettierd for TS/JS/React formatting
    - Disable tailwind-tools (incompatible with nvim 0.11)

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -15,6 +15,21 @@ return {
     },
     opts = {
       notify_on_error = false,
+      formatters = {
+        prettierd = {
+          condition = function(self, ctx)
+            return vim.fs.exists({
+              ctx.filename,
+              '.prettierrc',
+              '.prettierrc.json',
+              '.prettierrc.js',
+              '.prettierrc.mjs',
+              'prettier.config.js',
+              'prettier.config.mjs',
+            })
+          end,
+        },
+      },
       format_on_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
@@ -31,11 +46,13 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { 'prettierd' },
+        typescriptreact = { 'prettierd' },
+        javascript = { 'prettierd' },
+        javascriptreact = { 'prettierd' },
+        json = { 'prettierd' },
+        html = { 'prettierd' },
+        css = { 'prettierd' },
       },
     },
   },
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -231,9 +231,14 @@ return {
 				jdtls = {},
 				cssls = {},
 				jsonls = {},
+				sqls = {},
 				marksman = {},
 				bashls = {},
 				html = {},
+				sqls = {},
+				ts_ls = {},
+				tailwindcss = {},
+				eslint = {},
 
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
@@ -273,14 +278,15 @@ return {
 			--
 			-- You can add other tools here that you want Mason to install
 			-- for you, so that they are available from within Neovim.
-			local ensure_installed = vim.tbl_keys(servers or {})
-			vim.list_extend(ensure_installed, {
-				"stylua", -- Used to format Lua code
-				"jdtls",
-				"black",
-				"prettier",
-				"clang-format",
-			})
+		local ensure_installed = vim.tbl_keys(servers or {})
+		vim.list_extend(ensure_installed, {
+			"stylua",
+			"jdtls",
+			"black",
+			"prettierd",
+			"clang-format",
+			"sqls",
+		})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 			require("mason-lspconfig").setup({
